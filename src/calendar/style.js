ecc5aa86e3cedcdd1fafa1d6d6e384df0fe5371b
Vincent Nguyen <vinh.nglx@gmail.com>
2021-04-16 20:54:54 +0800

    Add padding bottom for the Calendar component

--- src/calendar/style.js
+++ src/calendar/style.js
@@ -9,7 +9,8 @@ export default function getStyle(theme = {}) {
     container: {
       paddingLeft: 5,
       paddingRight: 5,
-      backgroundColor: appStyle.calendarBackground
+      backgroundColor: appStyle.calendarBackground,
+      paddingBottom: 10
     },
     dayContainer: {
       flex: 1,
